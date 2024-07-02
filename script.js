6e85031a185613cf84db77246115b223f1eae913
PrammP <92103872+PrammP@users.noreply.github.com>
2024-07-02 09:37:16 +0200

    js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let sequence = [];
-let PlayerSequence = [];
-
 const start = document.getElementById("start");
 var info = document.getElementById("infos");
 
 function startGame() {
   start.style.display = "none";
   info.style.display = "block";
-  info.textContent = "Attendez la séquence...";
+  info.textContent = points;
+}
+
+let points = 0;
+
+function nextStep() {
+  const colors = [
+    "blue",
+    "red",
+    "yellow",
+    "lgreen",
+    "grey",
+    "orange",
+    "green",
+    "purple",
+    "brown",
+  ];
+  const random = colors[Math.floor(Math.random() * colors.length)];
+
+  return random;
 }
